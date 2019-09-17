18e082f66830a0d6f0739eeaf216dd26ab7f1733
Viktor Sheverdin <32000453+ViktorSheverdin@users.noreply.github.com>
2019-09-16 17:17:50 -0700

    Delete removeNAT.sh

--- removeNAT.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-vboxmanager () { VBoxManage.exe "$@"; }
-for network in $(vboxmanager natnetwork list)
-do
-	echo $network
-	#vboxmanager natnetworks remove --netname=$network
-done
-
