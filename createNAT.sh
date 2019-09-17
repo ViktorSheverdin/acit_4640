e6d59251f3d6b3819b3c0c99d043e3c34ec09b80
Viktor Sheverdin <32000453+ViktorSheverdin@users.noreply.github.com>
2019-09-16 17:17:41 -0700

    Delete createNAT.sh

--- createNAT.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash -x
-
-vboxmanager natnetwork add --netname net_4640 --network 192.168.250.0/24 --enable --dhcp off --ipv6 off --port-forward-4 "ssh:tcp:[]:50022:[192.168.250.10]:22" --port-forward-4 "http:tcp:[]:50080:[192.168.250.10]:80" --port-forward-4 "https:tcp:[]:50443:[192.168.250.10]:443"
