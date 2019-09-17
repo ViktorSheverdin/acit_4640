6297a89ba9932a64908a64516f8dcde31b57c7db
Viktor Sheverdin <32000453+ViktorSheverdin@users.noreply.github.com>
2019-09-16 17:17:59 -0700

    Delete showVM_Info.sh

--- showVM_Info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-vboxmanager () { VBoxManage.exe "$@"; }
-VM_NAME="VM_ACIT4640"
-SED_PROGRAM="/^Config file:/ { s/^.*:\s\+\(\S\+\)/\1/; s|\\\\|/|gp }"
-VBOX_FILE=$(vboxmanager showvminfo "$VM_NAME" | sed -ne "$SED_PROGRAM")
-VM_DIR=$(dirname "$VBOX_FILE")
-echo $VM_DIR
-
